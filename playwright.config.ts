90424f0615d0244af975c18888a65795845478e5
Shankar <shankarag66@gmail.com>
2025-08-27 15:08:37 +0530

    Fixed Alingment

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,12 +26,12 @@ export default defineConfig({
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
+    /* BaseURL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
-    video:'on'
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
